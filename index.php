3acb054a5dca570095ef90d1310e2bdccf3ca0bd
Reza Oktariana <oktariana.reza@gmail.com>
2023-11-20 17:10:18 +0700

    menambahkan list

--- index.php
+++ index.php
@@ -8,5 +8,9 @@
 <body>
     <h1>Hello World!</h1>
     <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Magnam et voluptate hic incidunt pariatur esse recusandae, voluptas unde ea consequuntur quod obcaecati cupiditate rerum officiis vitae modi excepturi dolore quibusdam.</p>
+    <ul>
+        <li>satu</li>
+        <li>dua</li>
+    </ul>
 </body>
 </html>
